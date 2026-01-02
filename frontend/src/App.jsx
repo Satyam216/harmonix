ad01d29921a055aab7d25406479a748ea2bb96a6
Satyam216 <satyamjain216@gmail.com>
2026-01-03 00:10:10 +0530

    Project Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-green-500">
+        HARMONIX
+      </h1>
+    </div>
+  );
+}
+
+export default App;
